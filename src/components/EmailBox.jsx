42e3b87b2d34a7a645c58a27b2818c299566dc48
deko95 <ks1379@scarletmail.rutgers.edu>
2023-05-11 20:33:17 -0400

    emailjs test

---
+++ src/components/EmailBox.jsx
@@ -0,0 +1,38 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+export default function EmailBox() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.PUBLIC_EMAILJS_SERVICE_ID,
+        process.env.PUBLIC_EMAILJS_TEMPLATE_ID,
+        form.current,
+        process.env.PUBLIC_EMAILJS_USER_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail} className="">
+      <label>Name</label>
+      <input className="text-black" type="text" name="name" />
+      <label>Email</label>
+      <input className="text-black" type="email" name="email" />
+      <label>Message</label>
+      <textarea className="text-black" name="message" />
+      <input type="submit" value="Send" className="cursor-pointer" />
+    </form>
+  );
+}
